d71d83a487a77dfcce944e4d821d598d869e7b0e
gmricardo <gmricardo3@gmail.com>
2025-09-21 22:42:38 -0500

    Add S3 backend configuration for Terraform state management

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
