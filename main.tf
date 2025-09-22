57273ea93c83b477c13ed4e6f31d7093928a73ba
gmricardo <gmricardo3@gmail.com>
2025-09-21 22:46:06 -0500

    Update S3 bucket name for Terraform backend configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-gmricardo3"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
